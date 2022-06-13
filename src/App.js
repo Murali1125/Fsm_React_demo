5f4b7682792ca820349f8178474dea6e98cd62fd
Murali Krishna <murali1125s@gmail.com>
2022-06-13 12:05:54 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> Hello Murali
-        <input placeholder="Enter name" />
-        {/* <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"> Hello Evora
+        {/* <input placeholder="Enter name" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
